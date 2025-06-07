f7137d06fb9bcf61446343b4b0085d686d0cfa30
Abhinav Singh <theawless@gmail.com>
2025-06-06 22:37:55 -0700

    Remove png files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.gobbledygook.theawless.faltuapp"
         minSdk = 21
         targetSdk = 35
-        versionCode = 10
+        versionCode = 11
         versionName = "1.0"
     }
 
